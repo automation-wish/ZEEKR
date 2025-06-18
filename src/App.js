8684d91456d097c68d2d612206f1e6a6c156ca01
unknown <automation.wish@gmail.com>
2025-06-18 09:53:13 +0300

    style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { Helmet } from "react-helmet";
 import ZeekrChatbot from "./ZeekrChatbot";
 
-function App() {
-  return <ZeekrChatbot />;
-}
+export default function App() {
+  return (
+    <>
+      <Helmet>
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,maximum-scale=1"
+        />
+      </Helmet>
 
-export default App;
+      <ZeekrChatbot />
+    </>
+  );
+}
